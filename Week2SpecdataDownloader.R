e19917cd8c7f2343fbcc0c517dd69ebdf63f4437
sherifffruitfly <sherifffruitfly@gmail.com>
2017-06-23 07:42:12 -0700

    incremental commit - download works, cleanup doesn't

--- Week2SpecdataDownloader.R
+++ Week2SpecdataDownloader.R
@@ -5,13 +5,39 @@
 #message to console after each step
 
 #download zip
-zipdataurl <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
-zipdatadesty <- "C:\\Users\\sheri\\Downloads\\specdata.zip"
+zipData.Url <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
+zipData.Folder <- "C:\\Users\\sheri\\Downloads\\classzip\\"
+zipDataFiles.Folder <- "temp"
+zipData.File <- "specdata.zip"
 
-#> URL      <- "http://rfunction.com/code/1202/120222.R"
-#> destfile <- "downloaded/120222.R"
-#> download.file(URL, destfile)
 
-download.file(zipdataurl, zipdatadesty)
+# one way or the other, we start with a clean directory
+if(!dir.exists(zipData.Folder))
+{
+  dir.create(zipData.Folder)
+  dir.create(paste(zipData.Folder, zipDataFiles.Folder))
+} else
+{
+  file.remove(dir(path=zipData.Folder, pattern="*.*"))
+}
 
-#test
+
+#download the zip data file
+zipData.FolderFile <- paste(zipData.Folder, zipData.File)
+download.file(zipData.Url, zipData.FolderFile)
+
+
+#unzip the zip data file into the data files subdir
+unzip(zipData.FolderFile, exdir=paste(zipData.Folder, zipDataFiles.Folder))
+
+
+
+# clean up by deleting everything we created
+unlink(zipData.Folder, recursive=FALSE)
+#delete all files in data file folder
+
+#delte the data file folder
+
+#delete all files in the zip folder
+
+#delete the zip folder
