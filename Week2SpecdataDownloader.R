466c45d8ed664ed81c877f77080cbd3e5bb2cdd4
sherifffruitfly <sherifffruitfly@gmail.com>
2017-07-06 10:51:00 -0700

    started cumulative_mean implementation

--- Week2SpecdataDownloader.R
+++ Week2SpecdataDownloader.R
@@ -1,3 +1,20 @@
+testfun1 <- function(x){x^2}
+testfun2 <- function(x, y){testfun1(x) * y}
+currmean <- 0
+currnum <- 0
+runningmean <- function(k, x_k, M_k1){
+  if(1==1)
+  {
+    
+  }
+}
+
+#
+# am i gonna do this running average file by file, or datapoint by datapoint?
+# ie point by point i can do the recursion k, xk, Mk-1
+# but file by file it makes sense to do weighted average of averages file by file - WeightedAverage(M1, k1, M2, k2)
+
+
 #data getter
 #this will dl the zip from coursera site
 #put zip in passed directory (create if needed)
@@ -6,44 +23,49 @@
 
 #download zip
 zipData.Url <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
-zipData.Folder <- "C:\\Users\\sheri\\Downloads\\classzip\\"
-zipDataFiles.Folder <- "temp"
+zipData.Folder <- "C:\\classzip"
+zipDataFiles.Folder <- "\\temp"
 zipData.File <- "specdata.zip"
+zipData.PathFile <- paste(zipData.Folder, "\\", zipData.File, sep="")
+zipData.TempFolder <- paste(zipData.Folder, zipDataFiles.Folder, sep="")
+
+# # one way or the other, we start with a clean directory
+# if(dir.exists(zipData.Folder))
+# {
+#   print(paste("data directories already exist - wiping files for a clean start: ", zipData.Folder))
+#   #file.remove(dir(path=zipData.Folder, pattern="*.*"))
+#   unlink(zipData.Folder, recursive=TRUE)
+# }
+# 
+# print(paste("creating zipdata.folder: ", zipData.Folder))
+# dir.create(zipData.Folder)
+# 
+# print(paste("creating zipdatafiles.folder subdir: ", zipDataFiles.Folder))
+# dir.create(paste(zipData.Folder, zipDataFiles.Folder, sep=""))
+# 
+# 
+# 
+# #download the zip data file
+# zipData.FolderFile <- paste(zipData.Folder, "\\", zipData.File)
+# print(paste("downloading data: ", zipData.FolderFile, sep=""))
+# download.file(zipData.Url, zipData.FolderFile)
+# 
+# 
+# #unzip the zip data file into the data files subdir
+# cat(paste("unzipping: \n",  zipData.Folder, zipDataFiles.Folder, sep=""))
+# unzip(zipData.FolderFile, exdir=paste(zipData.Folder, zipDataFiles.Folder))
+
+#delete all files from temp\ directory
+unlink(paste(zipData.TempFolder, "\\*", sep=""))
+
+#unzip the data files into temp\ directory
+unzip(zipData.PathFile, exdir=zipData.TempFolder)
 
 
-# one way or the other, we start with a clean directory
-if(!dir.exists(zipData.Folder))
-{
-  print(paste("creating zipdata.folder: ", zipData.Folder))
-  dir.create(zipData.Folder)
-  
-  print(paste("creating zipdatafiles.folder subdir: ", zipDataFiles.Folder))
-  dir.create(paste(zipData.Folder, zipDataFiles.Folder))
-  
-  print("all done making directories")
-} else
-{
-  print("data directories already exist - wiping files for a clean start")
-  file.remove(dir(path=zipData.Folder, pattern="*.*"))
-}
-
-
-#download the zip data file
-zipData.FolderFile <- paste(zipData.Folder, zipData.File)
-download.file(zipData.Url, zipData.FolderFile)
-
-
-#unzip the zip data file into the data files subdir
-unzip(zipData.FolderFile, exdir=paste(zipData.Folder, zipDataFiles.Folder))
 
 
 
+
+#COMMENT THIS OUT UNTIL ALL DONE AND NEEDED
 # clean up by deleting everything we created
-unlink(zipData.Folder, recursive=FALSE)
-#delete all files in data file folder
-
-#delte the data file folder
-
-#delete all files in the zip folder
-
-#delete the zip folder
+#unlink(zipData.Folder, recursive=TRUE)
---
+++ moving_mean.R
@@ -0,0 +1,32 @@
+#this looks like the central implementation
+#need to figure out how to feed a vector to it sequentially w/ results
+
+cumulative_mean <- function(prev_step=c(1, 0), new_data=0)
+{
+  #prev_step[1] = current time tick step
+  #prev_step[2] = previous mean
+  
+  new_mean <- ((prev_step[1] - 1) * prev_step[2] /prev_step[1] ) + ((1/prev_step[1]) * new_data)
+  new_tick <- prev_step[1] + 1
+  
+  result <- c(new_tick, new_mean)
+  result
+}
+
+
+mm_control <- function(vector_data=0)
+{
+  #temp hardcoding the argument
+  data = c(3, 7, 4, 2)
+  
+  r1 <- cumulative_mean(c(1, 0), data[1])
+  r2 <- cumulative_mean(r1, data[2])
+  r3 <- cumulative_mean(r2, data[3])
+  r4 <- cumulative_mean(r3, data[4])
+  
+  print(r1)
+  print(r2)
+  print(r3)
+  print(r4)
+  
+}
