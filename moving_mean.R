0fea929f144a4fdf082410fdbd659247a78b1635
sherifffruitfly <sherifffruitfly@gmail.com>
2017-07-06 17:28:57 -0700

    home update

--- moving_mean.R
+++ moving_mean.R
@@ -1,4 +1,3 @@
-#this looks like the central implementation
 #need to figure out how to feed a vector to it sequentially w/ results
 
 cumulative_mean <- function(prev_step=c(1, 0), new_data=0)
@@ -6,7 +5,7 @@ cumulative_mean <- function(prev_step=c(1, 0), new_data=0)
   #prev_step[1] = current time tick step
   #prev_step[2] = previous mean
   
-  new_mean <- ((prev_step[1] - 1) * prev_step[2] /prev_step[1] ) + ((1/prev_step[1]) * new_data)
+  new_mean <- ((prev_step[1] - 1) / prev_step[1] * prev_step[2]) + ((1/prev_step[1]) * new_data)
   new_tick <- prev_step[1] + 1
   
   result <- c(new_tick, new_mean)
@@ -14,12 +13,12 @@ cumulative_mean <- function(prev_step=c(1, 0), new_data=0)
 }
 
 
-mm_control <- function(vector_data=0)
+cm_control <- function(vector_data=0)
 {
   #temp hardcoding the argument
   data = c(3, 7, 4, 2)
   
-  r1 <- cumulative_mean(c(1, 0), data[1])
+  r1 <- cumulative_mean(new_data=data[1])
   r2 <- cumulative_mean(r1, data[2])
   r3 <- cumulative_mean(r2, data[3])
   r4 <- cumulative_mean(r3, data[4])
