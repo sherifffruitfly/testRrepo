646f0e1d6b9fe587dbfd369251b611a2368ee0c6
sherifffruitfly <sherifffruitfly@gmail.com>
2017-07-06 22:25:47 -0700

    got the vector data feed and recursion working
    next step is to pass only 1 data element thru recursion steps rather than the entire tail

--- moving_mean.R
+++ moving_mean.R
@@ -1,31 +1,22 @@
-#need to figure out how to feed a vector to it sequentially w/ results
-
-cumulative_mean <- function(prev_step=c(1, 0), new_data=0)
+cumulative_mean <- function(prev_step=c(1, 0), new_data=c())
 {
-  #prev_step[1] = current time tick step
-  #prev_step[2] = previous mean
+  # easy sample data argument: new_data=sample(0:100,100,replace=T)
   
-  new_mean <- ((prev_step[1] - 1) / prev_step[1] * prev_step[2]) + ((1/prev_step[1]) * new_data)
-  new_tick <- prev_step[1] + 1
-  
-  result <- c(new_tick, new_mean)
-  result
-}
-
-
-cm_control <- function(vector_data=0)
-{
-  #temp hardcoding the argument
-  data = c(3, 7, 4, 2)
-  
-  r1 <- cumulative_mean(new_data=data[1])
-  r2 <- cumulative_mean(r1, data[2])
-  r3 <- cumulative_mean(r2, data[3])
-  r4 <- cumulative_mean(r3, data[4])
-  
-  print(r1)
-  print(r2)
-  print(r3)
-  print(r4)
-  
+  if (length(new_data) == 0)
+  {
+    return("done!")
+  }
+  else
+  {
+    #prev_step[1] = current time tick step
+    #prev_step[2] = previous mean
+    
+    new_mean <- ((prev_step[1] - 1) / prev_step[1] * prev_step[2]) + ((1/prev_step[1]) * new_data[1])
+    new_tick <- prev_step[1] + 1
+    
+    result <- c(prev_step[1], new_data[1], new_mean)
+    print(result)
+    
+    return(cumulative_mean(c(new_tick, new_mean), new_data[-1]))
+  }
 }
