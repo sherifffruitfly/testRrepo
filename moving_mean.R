48d4bb169c8ad5510b4c6bef176e469e1b6350b7
sherifffruitfly <sherifffruitfly@gmail.com>
2017-07-12 08:25:54 -0700

    cum mean functions now working - perf is acceptable up to 10^6 input. obviously nowhere near as good as the internal MEAN function, but adequate for my purpose.

--- moving_mean.R
+++ moving_mean.R
@@ -1,15 +1,15 @@
-cumulative_mean <- function(prev_step=c(1, 0), new_data=c())
+cumulative_mean <- function(prev_step=c(1, 0), data=c())
 {
   # easy sample data argument: cumulative_mean(new_data=sample(0:100,100,replace=T))
 
-  if (length(new_data) > 0)
+  if (length(data) > 0)
   {
     #prev_step[1] = current time tick step
     #prev_step[2] = previous mean
     
-    if (!is.na(new_data[1]))
+    if (!is.na(data[1]))
     {
-      new_mean <- ((prev_step[1] - 1) / prev_step[1] * prev_step[2]) + ((1/prev_step[1]) * new_data[1])
+      new_mean <- ((prev_step[1] - 1) / prev_step[1] * prev_step[2]) + ((1/prev_step[1]) * data[1])
       new_tick <- prev_step[1] + 1
     }
     else
@@ -17,10 +17,10 @@ cumulative_mean <- function(prev_step=c(1, 0), new_data=c())
       new_tick <- prev_step[1]
       new_mean <- prev_step[2]
     }
-    result <- c(prev_step[1], new_data[1], new_mean)
+    result <- c(prev_step[1], data[1], new_mean)
     #print(result)
     
-    return(cumulative_mean(c(new_tick, new_mean), new_data[-1]))
+    return(cumulative_mean(c(new_tick, new_mean), data[-1]))
   }  
   else
   {
@@ -41,18 +41,31 @@ CM_control <- function(data=c(), mr=floor(sqrt(length(data))))
   
   options(warn = oldw)
   
-  print(data_matrix)
+  #print(data_matrix)
+  
+  start.time <- Sys.time()
   
   seed <- c(1, 0)
   result_list <- c()
   for (i in 1:ncol(data_matrix))
   {
+    #print(c("seed at start of loop: ", c(i, seed)))
+    
     seed <- cumulative_mean(seed, data_matrix[,i])
     result_list <- c(result_list, c(seed))
     
-    print(c(i, seed))
+    #need to advance the tick
+    seed[1] <- seed[1]+1
+    
+    #print(c("seed at end of loop: ", c(i, seed)))
   next
   }
+  
+  end.time <- Sys.time()
+  time.taken <- end.time - start.time
+  print(paste("ET: ", time.taken))
+  
+  return(seed)
     
-  print(matrix(result_list, ncol=2, byrow=TRUE))
+  #print(matrix(result_list, ncol=2, byrow=TRUE))
 }
